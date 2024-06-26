881349b580396f91d12857c8e06bc30f87b013eb
3n952 <smrlatks@ajou.ac.kr>
2024-06-26 10:19:32 +0900

    array

---
+++ 07Two_sum.py
@@ -0,0 +1,26 @@
+'''
+https://leetcode.com/problems/two-sum/
+
+1. 덧셈하여 타겟을 만들 수 있는 배열의 두 숫자 인덱스를 리턴
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
